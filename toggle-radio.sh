74cc0803b4f16067a1c68e4513704e6d4b5b20b9
Tom Brewe <tom@nylkiway.net>
2021-12-28 17:30:25 +0100

    start/stop systemd-service instead of enable/disable

--- toggle-radio.sh
+++ toggle-radio.sh
@@ -4,10 +4,10 @@ systemctl --user -q is-active radio.service
  
 if [ $? -eq 0 ]; then
     echo "radio toggled: radio.service is running, so turn radio turn OFF."
-    systemctl --user disable --now radio.service
+    systemctl --user stop radio.service
     exit 1
 else
     echo "radio toggled: radio.service is not running, so turn radio turn ON."
-    systemctl --user enable --now radio.service
+    systemctl --user start radio.service
     exit 1
 fi

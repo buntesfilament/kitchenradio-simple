738ed2c5665413872efb971823bff4fd4a6e30a9
Tom Brewe <tom@nylkiway.net>
2021-12-28 16:38:45 +0100

    fix: add --user to systemctl commands

--- radio-toggle.sh
+++ radio-toggle.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if (systemctl -q is-active radio.service); then
+if (systemctl --user -q is-active radio.service); then
     echo "radio toggled: radio.service is running, so turn radio turn OFF."
-    systemctl disable --now radio.service
+    systemctl --user disable --now radio.service
     exit 1
 else
     echo "radio toggled: radio.service is not running, so turn radio turn ON."
-    systemctl enable --now radio.service
+    systemctl --user enable --now radio.service
     exit 1
 fi

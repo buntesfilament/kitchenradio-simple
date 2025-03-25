70861d5dddbf0016b0c9ff9e9087510a0f0d3ac2
André <poinck@users.noreply.github.com>
2025-03-25 16:13:57 +0100

    stop after 4 hours of playback

--- radio.sh
+++ radio.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-mpv --network-timeout=120 --volume=100 --quiet --no-video https://stream.radiofrance.fr/fip/fip_hifi.m3u8\?id\=radiofrance
+mpv --network-timeout=120 --volume=100 --quiet --no-video https://stream.radiofrance.fr/fip/fip_hifi.m3u8\?id\=radiofrance &
+sleep 14400 # 4h
+sudo systemctl stop kitchenradio-fip.service

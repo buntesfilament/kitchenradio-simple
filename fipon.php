3a9aaf5b3e96bee4c667d92ccbafccbe99e8555a
André <poinck@users.noreply.github.com>
2025-03-25 15:01:32 +0100

    add php webservice to start systemd service
    warning, not meant for production use.

---
+++ fipon.php
@@ -0,0 +1,15 @@
+<?php
+$command = 'sudo /bin/systemctl start kitchenradio-fip.service';
+
+// execute the command
+$output = [];
+$returnVar = 0;
+exec($command, $output, $returnVar);
+
+// check the result
+if ($returnVar === 0) {
+    echo "Command executed successfully: " . implode("\n", $output);
+} else {
+    echo "Error executing command: " . implode("\n", $output);
+}
+?>
